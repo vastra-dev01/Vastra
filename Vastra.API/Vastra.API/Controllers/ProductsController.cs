9b02570b8fb6b46910fd78bd5fc4847f43b56880
Sumit Ranjan <jcsumit917@gmail.com>
2023-06-19 13:55:44 +0530

    Added products controller

---
+++ Vastra.API/Vastra.API/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vastra.API.Services;
+
+namespace Vastra.API.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IVastraRepository _vastraRepository;
+        public ProductsController(IVastraRepository vastraRepository)
+        {
+            _vastraRepository = vastraRepository;
+        }
+
+    }
+}
