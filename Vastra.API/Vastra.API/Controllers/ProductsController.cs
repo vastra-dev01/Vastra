d8e0d9568d6a05b281c76820a314bc2eaac255ae
Sumit Ranjan <jcsumit917@gmail.com>
2023-06-26 17:47:24 +0530

    Add Action Methods

--- Vastra.API/Vastra.API/Controllers/ProductsController.cs
+++ Vastra.API/Vastra.API/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Vastra.API.Models;
 using Vastra.API.Services;
 
 namespace Vastra.API.Controllers
@@ -8,10 +11,36 @@ namespace Vastra.API.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly IVastraRepository _vastraRepository;
-        public ProductsController(IVastraRepository vastraRepository)
+        private readonly IMapper _mapper;
+        const int maxProductsPageSize = 20;
+        public ProductsController(IVastraRepository vastraRepository, IMapper mapper)
         {
             _vastraRepository = vastraRepository;
+            _mapper = mapper;
         }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetProducts(
+            string? name, string? searchQuery, int pageNumber = 1, int pageSize = 10)
+        {
+            if (pageSize > maxProductsPageSize)
+            {
+                pageSize = maxProductsPageSize;
+            }
+            var (productEntities, paginationMetadata) = await _vastraRepository.GetProductsAsync(
+                name, searchQuery, pageNumber, pageSize);
 
+            Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(paginationMetadata));
+            return Ok(_mapper.Map<IEnumerable<ProductDto>>(productEntities));
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult>GetProduct(int id)
+        {
+            var product = await _vastraRepository.GetProductAsync(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<ProductDto>(product));
+        }
     }
 }
--- Vastra.API/Vastra.API/Program.cs
+++ Vastra.API/Vastra.API/Program.cs
@@ -1,4 +1,5 @@
 using Vastra.API.DBContexts;
+using Vastra.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<VastraContext>();
+builder.Services.AddScoped<IVastraRepository, VastraRepository>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
