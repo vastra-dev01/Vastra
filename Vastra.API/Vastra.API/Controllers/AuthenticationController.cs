ea76f96377337a6c5f4194b23ebde132265a0688
Sumit Ranjan <jcsumit917@gmail.com>
2023-07-28 17:40:13 +0530

    WIP : Implement Authentication

---
+++ Vastra.API/Vastra.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vastra.API.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+    }
+}
---
+++ Vastra.API/Vastra.API/Models/Authentication/AuthenticationRequestBody.cs
@@ -0,0 +1,8 @@
+﻿namespace Vastra.API.Models.Authentication
+{
+    public class AuthenticationRequestBody
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
