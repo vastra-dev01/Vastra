125458d86486b479de3f674ed529dc1333618e78
Sumit Ranjan <jcsumit917@gmail.com>
2023-08-16 17:36:00 +0530

    Implement Address Controller

--- Vastra.API/Vastra.API/Controllers/AddressesController.cs
+++ Vastra.API/Vastra.API/Controllers/AddressesController.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.XPath;
 using Vastra.API.Entities;
+using Vastra.API.Enums;
 using Vastra.API.Models;
 using Vastra.API.Models.ForCreationAndUpdate;
 using Vastra.API.Services;
@@ -90,7 +93,22 @@ namespace Vastra.API.Controllers
 
             finaladdress.DateAdded = DateTime.Now;
             finaladdress.DateModified = DateTime.Now;
+
+            var (addresses, paginationMetadata) = await _vastraRepository.GetAddressesForUserAsync(userId, 1, 100);
+            if (addresses.Count() == 0)
+            {
+                finaladdress.Type = (int)AddressType.Primary;
+            }
             await _vastraRepository.AddAddressForUserAsync(userId, finaladdress);
+            
+            //if address set as primary, set other addresses as secondary
+            if (finaladdress.Type == (int)AddressType.Primary && addresses.Count() > 0)
+            {
+                foreach (var addressMember in addresses)
+                {
+                    addressMember.Type = (int)AddressType.Secondary;
+                }
+            }
             await _vastraRepository.SaveChangesAsync();
 
             var createdAddressToReturn = _mapper.Map<AddressDto>(finaladdress);
@@ -105,8 +123,91 @@ namespace Vastra.API.Controllers
 
                 );
         }
-
-        [HttpDelete]
+        [Authorize]
+        [HttpPatch("{addressId}")]
+        public async Task<ActionResult> PartiallyUpdateAddress(int roleId, int userId, int addressId,
+            JsonPatchDocument<AddressForUpdateDto> patchDocument)
+        {
+            if (!await _vastraRepository.RoleExistsAsync(roleId))
+            {
+                return NotFound();
+            }
+            if (!await _vastraRepository.UserExistsWithRoleAsync(roleId, userId))
+            {
+                return NotFound();
+            }
+            if (!await _vastraRepository.ValidateUserClaim(User, userId))
+            {
+                return Forbid();
+            }
+            var addressEntity = await _vastraRepository.GetAddressForUserAsync(userId, addressId);
+            if(addressEntity == null)
+            {
+                return NotFound();
+            }
+            var addressToPatch = _mapper.Map<AddressForUpdateDto>(addressEntity);
+            patchDocument.ApplyTo(addressToPatch, ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (!TryValidateModel(addressToPatch))
+            {
+                return BadRequest(ModelState);
+            }
+            _mapper.Map(addressToPatch, addressEntity);
+            //update Modified Time of address
+            addressEntity.DateModified = DateTime.Now;
+            
+            //if address set as primary, set other addresses as secondary
+            if(addressEntity.Type == (int)AddressType.Primary)
+            {
+                var (addresses, paginationMetadata) = await _vastraRepository.GetAddressesForUserAsync(userId,1,100);
+                foreach (var address in addresses)
+                {
+                    address.Type = (int)AddressType.Secondary;
+                }
+            }
+            await _vastraRepository.SaveChangesAsync();
+            return NoContent();
+        }
+        [Authorize]
+        [HttpPut("{addressId}")]
+        public async Task<ActionResult> UpdateAddress(int roleId, int userId, int addressId, AddressForUpdateDto address)
+        {
+            if (!await _vastraRepository.RoleExistsAsync(roleId))
+            {
+                return NotFound();
+            }
+            if (!await _vastraRepository.UserExistsWithRoleAsync(roleId, userId))
+            {
+                return NotFound();
+            }
+            if (!await _vastraRepository.ValidateUserClaim(User, userId))
+            {
+                return Forbid();
+            }
+            var addressEntity = await _vastraRepository.GetAddressForUserAsync(userId, addressId);
+            if (addressEntity == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(address, addressEntity);
+            //update Modified Time of product
+            addressEntity.DateModified = DateTime.Now;
+            //if address set as primary, set other addresses as secondary
+            if (addressEntity.Type == (int)AddressType.Primary)
+            {
+                var (addresses, paginationMetadata) = await _vastraRepository.GetAddressesForUserAsync(userId, 1, 100);
+                foreach (var addressMember in addresses)
+                {
+                    addressMember.Type = (int)AddressType.Secondary;
+                }
+            }
+            await _vastraRepository.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpDelete("{addressId}")]
         [Authorize]
         public async Task<ActionResult> DeleteAddress(int roleId, int userId, int addressId)
         {
